308833bddf552804949e009f7ae59a63016e6638
unknown <matthewaomalley@lewisu.edu>
2022-02-17 12:55:23 -0600

    Initial files added and testing

---
+++ index.js
@@ -0,0 +1,16 @@
+//Web app with Node.js and express for assignment portfolio
+
+// use express and create app object
+const express = require('express')
+const app = express()
+
+// define directory for files to server
+app.use(express.static(__dirname + '/public'))
+
+// define the port
+const port = process.env.PORT || 8080;
+
+// listen on the port
+app.listen(port, () => console.log(
+    `Express started on http://localhost:${port}; ` +
+    `press Ctrl-C to terminate.`))
