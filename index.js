72443f512ddca9e4d87d1990a356af3ba0e4c4a1
unknown <matthewaomalley@lewisu.edu>
2022-02-18 09:48:37 -0600

    final fix, json edit

--- index.js
+++ index.js
@@ -5,10 +5,12 @@ const express = require('express')
 const app = express()
 
 // define directory for files to server
-app.use(express.static(__dirname + '/public'))
+app.use(express.static(__dirname + '/public/assignment-portfolio'))
 
 // define the port
 const port = process.env.PORT || 8080;
+
+app.get('/', (req, res) => res.render('assignment-portfolio'))
 
 // listen on the port
 app.listen(port, () => console.log(
